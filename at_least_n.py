b862b8fe9f4e0f1825e58177d3d36299aa65e190
Travis Chen <travis.chen@everchanging.dev>
2024-03-12 21:24:15 -0700

    Handle base case when n == 1
    The bug was in the base case when n = 1.

--- at_least_n.py
+++ at_least_n.py
@@ -17,28 +17,45 @@ class AtLeastN:
     self.probabilities = list(probabilities)
 
   def compute(self, n: int) -> float:
+    """Computes the probability that at least n > 0 items resolve to true."""
+    if n < 1:
+      raise ValueError('n must be greater than 0')
+
     return self._n_or_more(len(self.probabilities) - 1, n)
 
   def _zero_condition(self, m: int, n: int) -> bool:
     """Returns true if these parameters should return probability 0.0."""
     # m + 1 means number of items in the list; if that is less than n, then
-    # there are not enough elementsto resolve at least n items.
+    # there are not enough elements to resolve at least n items.
     not_enough_elements = m + 1 < n
 
     # If there are no elements in the list, then we cannot resolve any.
-    no_elements_left = m == 0
+    no_elements_left = m < 0
 
-    # If n is negative, then we don't want to consider this item. This exists
-    # to calculate the case when n is 0.
-    ignored = n < 0
-
-    return not_enough_elements or no_elements_left or ignored
+    return not_enough_elements or no_elements_left
 
   @functools.lru_cache
   def _all_zero(self, m: int) -> float:
-    """Returns the probability of the first m items resolving false."""
-    return functools.reduce(lambda p, q: p * (1.0 - q), self.probabilities[:m],
-                            1.0)
+    """Returns the probability that all items 0 to m all resolve to false."""
+    if m == 0:
+      return 1.0 - self.probabilities[m]
+
+    return functools.reduce(lambda p, q: p * (1.0 - q),
+                            self.probabilities[:m + 1], 1.0)
+
+  @functools.lru_cache
+  def _at_least_one(self, m: int) -> float:
+    """Returns the probability that at least one from 0 to m resolves true."""
+    return 1.0 - self._all_zero(m)
+
+  @functools.lru_cache
+  def _exactly_one(self, m: int) -> float:
+    """Returns the probability that exactly one from 0 to m resolves true."""
+    if m == 0:
+      return self.probabilities[m]
+
+    return self.probabilities[m] * self._all_zero(m - 1) + (
+        1.0 - self.probabilities[m]) * self._exactly_one(m - 1)
 
   @functools.lru_cache
   def _exactly_n(self, m: int, n: int) -> float:
@@ -48,22 +65,18 @@ class AtLeastN:
       m: The index to start looking from (backwards).
       n: The number of items to resolve to true.
     """
-    if m == 0 and n == 1:
-      return self.probabilities[m]
-
     if self._zero_condition(m, n):
       return 0.0
 
     if n == 1:
-      return self.probabilities[m] * self._all_zero(m) + (
-          1 - self.probabilities[m]) * self._exactly_n(m - 1, n)
+      return self._exactly_one(m)
 
-    return self.probabilities[m] * self._exactly_n(
-        m - 1, n - 1) + (1 - self.probabilities[m]) * self._exactly_n(m - 1, n)
+    return self.probabilities[m] * self._exactly_n(m - 1, n - 1) + (
+        1.0 - self.probabilities[m]) * self._exactly_n(m - 1, n)
 
   @functools.lru_cache
   def _n_or_more(self, m: int, n: int) -> float:
-    """Returns the probability of n or more resolving to true.
+    """Returns the probability of at least n > 0 resolving to true.
 
     Args:
       m: The index to start looking from (backwards).
@@ -72,8 +85,8 @@ class AtLeastN:
     if self._zero_condition(m, n):
       return 0.0
 
-    if n < 1:
-      return max(self.probabilities[:m])
+    if n == 1:
+      return self._at_least_one(m)
 
     return self.probabilities[m] * self._exactly_n(
         m - 1, n - 1) + self._n_or_more(m - 1, n)
@@ -81,7 +94,3 @@ class AtLeastN:
 
 def at_least_n(probabilities: Iterable[float], n: int) -> float:
   return AtLeastN(probabilities).compute(n)
-
-
-if __name__ == '__main__':
-  print(at_least_n([1, 1], 1)) # currently a bug
